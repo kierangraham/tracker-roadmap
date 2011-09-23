96344d0ab9247e72306e8d4730476e36ff49e6a0
Kieran Graham <me@kierangraham.com>
2011-09-23 01:47:47 +0100

    skipping auth in development

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -61,6 +61,7 @@ protected
   end
 
   def authenticate
+    return true if Rails.env.development?
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["USERNAME"] && password == ENV["PASSWORD"]
     end

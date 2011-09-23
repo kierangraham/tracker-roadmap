d2e7b7a5a3f69e49866b3ae092d2ccabb61fe552
Kieran Graham <me@kierangraham.com>
2011-09-23 03:01:12 +0100

    Revert "adding tasks remaining and percent complete stats"
    This reverts commit 4d7ea96dcbccfddafa2b9d20cd7a46319d2e1fdb.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -54,7 +54,6 @@ protected
         if story.story_type == "release"
           release = story
           release.estimated_finish = iteration.finish
-          release.tasks_remaining = iteration.stories.size
           @releases << release
         end
       end
--- config/initializers/tracker_story.rb
+++ config/initializers/tracker_story.rb
@@ -1,7 +1,5 @@
 module PivotalTracker
   class Story
     attr_accessor :estimated_finish
-    attr_accessor :tasks_remaining
-    attr_accessor :percent_complete
   end
 end

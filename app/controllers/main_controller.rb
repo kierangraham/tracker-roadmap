96787c4096a1272e2d1e465a7a30cb4a5b469fb4
Kieran Graham <me@kierangraham.com>
2011-09-23 01:47:31 +0100

    cleaning whitespace

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,43 +1,43 @@
 class MainController < ApplicationController
   before_filter :authenticate
-  
+
   def index
     collect_releases
-    
+
   end
-  
+
   def workstream
     collect_releases
-    
+
     releases_sorted_by_date = @releases.select{|x| x if x.deadline }.sort{|x, y| x.deadline <=> y.deadline }
-    
-    
-    # Owners = Workstreams 
+
+
+    # Owners = Workstreams
     # Gather owners and months for all releases
     @owners = @releases.collect{|x| x.owned_by}.uniq
     @months = (releases_sorted_by_date.first.deadline..releases_sorted_by_date.last.deadline).collect{|y| [y.month, y.strftime('%b, %Y')] }.uniq
-    
+
     # EX: { 11 => { "Bob Smith" => [release, release], "Timmy Tots" => [release], "Jane Robbins" => [release, release, release] } }
     @releases_by_deadline_month = {}
-    
+
     # Prepare set
-    @months.each do |m| 
+    @months.each do |m|
       releases_by_owner = {}
       @owners.each{|y| releases_by_owner[y] = [] }
       @releases_by_deadline_month[m[0]] = releases_by_owner
     end
-    
-    releases_sorted_by_date.each do |release| 
+
+    releases_sorted_by_date.each do |release|
       @releases_by_deadline_month[release.deadline.month][release.owned_by] << release
     end
-    
+
     @unscheduled_releases_by_owner = {}
-    @owners.each do |owner| 
+    @owners.each do |owner|
       @unscheduled_releases_by_owner[owner] = @releases.select{|x| x if !x.deadline && x.owned_by == owner }
     end
-    
+
     @col_size = ( (958 - (@owners.size * 8)) / @owners.size).to_i
-    
+
   end
 
 protected
@@ -45,7 +45,7 @@ protected
   def collect_releases
     PivotalTracker::Client.token = ENV["TRACKER_API_KEY"]
     PivotalTracker::Client.use_ssl = true
-    
+
     @project = PivotalTracker::Project.find(ENV["TRACKER_PROJECT_ID"].to_i)
     @releases = []
     @iterations = params[:all] ? @project.iterations.all : [PivotalTracker::Iteration.current(@project)] + PivotalTracker::Iteration.backlog(@project)
@@ -59,7 +59,7 @@ protected
       end
     end
   end
-  
+
   def authenticate
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["USERNAME"] && password == ENV["PASSWORD"]

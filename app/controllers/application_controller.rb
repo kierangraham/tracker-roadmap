1dd7a6c07563d30237cf715f9b07a76ef37d0a77
Kieran Graham <me@kierangraham.com>
2011-10-15 05:02:33 +0100

    adding cache headers to roadmap app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  after_filter :set_cache_headers
+
+  protected
+
+  def set_cache_headers
+    response.headers['Cache-Control'] = 'public, max-age=300'
+  end
 end
